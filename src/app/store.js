3b677d04bf95d1b07fcf76904f797d247deed4e8
Fahim <fahimalemroz@gmail.com>
2021-09-21 14:48:26 +0600

    Fixed an importing path issue

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import todosReducer from '../features/todosReducer/todosSlice';
+import todosReducer from '../features/todos/todosSlice';
 
 export const store = configureStore({
 	reducer: {
-		todos: todosReducer
+		todos: todosReducer,
 	},
 });
