11265411c8fe4ad88e14511ce07a161d61fb2181
Fahim <fahimalemroz@gmail.com>
2021-09-24 19:39:34 +0600

    Started working on the addTodo functionality

--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,9 +1,26 @@
+import { useState } from 'react';
+
 function AppNav() {
+	const [todo, setTodo] = useState('');
+
+	const addTodo = (params) => {
+		console.log(todo);
+		setTodo('');
+	};
+
 	return (
 		<nav>
 			<button className="clear-btn">Clear</button>
-			<input className="text-center" type="text" placeholder="Add Todo" />
-			<button className="add-btn">Add</button>
+			<input
+				className="text-center"
+				type="text"
+				value={todo}
+				placeholder="Add Todo"
+				onChange={(e) => setTodo(e.target.value)}
+			/>
+			<button className="add-btn" onClick={addTodo}>
+				Add
+			</button>
 		</nav>
 	);
 }
