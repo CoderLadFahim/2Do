3d1bedbb0495d221cef95afd0f5bcc2228f881fb
Fahim <fahimalemroz@gmail.com>
2021-09-24 19:53:55 +0600

    TodoAdding and clearing functionality added

--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,24 +1,36 @@
+import { useDispatch } from 'react-redux';
+import { addTodo, clearTodos } from '../features/todos/todosSlice';
 import { useState } from 'react';
 
 function AppNav() {
+	const dispatch = useDispatch();
 	const [todo, setTodo] = useState('');
 
-	const addTodo = (params) => {
-		console.log(todo);
+	const todoAdder = () => {
+		if (!todo) return;
+		const todoObj = { text: todo, id: Date.now() };
+		dispatch(addTodo(todoObj));
+
+		// setting input value to be en emtpy string after user adds a todo
 		setTodo('');
 	};
 
 	return (
 		<nav>
-			<button className="clear-btn">Clear</button>
+			<button className="clear-btn" onClick={() => dispatch(clearTodos())}>
+				Clear
+			</button>
 			<input
 				className="text-center"
 				type="text"
 				value={todo}
 				placeholder="Add Todo"
 				onChange={(e) => setTodo(e.target.value)}
+				onKeyUp={(e) =>
+					e.target.value && e.keyCode === 13 ? todoAdder() : ''
+				}
 			/>
-			<button className="add-btn" onClick={addTodo}>
+			<button className="add-btn" onClick={todoAdder}>
 				Add
 			</button>
 		</nav>
--- src/features/todos/todosSlice.js
+++ src/features/todos/todosSlice.js
@@ -8,12 +8,17 @@ export const todosSlice = createSlice({
 	name: 'todos',
 	initialState,
 	reducers: {
-		addTodo: (state, action) => {},
+		addTodo: (state, { payload }) => {
+			state.todos = [...state.todos, payload];
+		},
 		deleteTodo: (state, action) => {},
+		clearTodos: (state, action) => {
+			state.todos = [];
+		},
 	},
 });
 
 // Action creators are generated for each case reducer function
-export const {addTodo, deleteTodo} = todosSlice.actions;
+export const { addTodo, deleteTodo, clearTodos } = todosSlice.actions;
 
 export default todosSlice.reducer;
