9cf9a0c1fbc7a068fd41b86cadeef23343569032
Fahim <fahimalemroz@gmail.com>
2021-09-24 22:22:53 +0600

    Todo markup added

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,2 +1,14 @@
 import { faTrash, faCheck, faRedoAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+function Todo({ todo }) {
+	return (
+		<div className="todo">
+			<p>TODO_TEXT</p>
+			<FontAwesomeIcon icon={todo.isCompleted ? faRedoAlt : faCheck} />
+			<FontAwesomeIcon icon={faTrash} />
+		</div>
+	);
+}
+
+export default Todo;
