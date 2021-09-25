e18ef40aea678d6c3f2a23b2f985c24f9914dfc9
Fahim <fahimalemroz@gmail.com>
2021-09-25 22:53:58 +0600

    Fixed the TTS base styles

--- src/components/TodoTypeSelector.jsx
+++ src/components/TodoTypeSelector.jsx
@@ -1,27 +1,33 @@
 function TodoTypeSelector({ activeViewType, viewTypeUpdater }) {
+	const selectorClasses = 'flex-1 text-center py-2';
+
 	return (
-		<ul className="flex list-none w-1/2 py-2" data-testid="type-selector">
+		<ul
+			className="flex list-none w-4/6  bg-gray-700 nunito rounded-lg shadow-md
+			overflow-hidden"
+			data-testid="type-selector"
+		>
 			<li
 				onClick={(e) => viewTypeUpdater(e.target.textContent.toLowerCase())}
 				className={`${
-					activeViewType === 'all' ? 'bg-blue-400' : ''
-				}  flex-1 text-center h-full`}
+					activeViewType === 'all' ? 'bg-blue-400 font-bold' : ''
+				}  ${selectorClasses}`}
 			>
 				All
 			</li>
 			<li
 				onClick={(e) => viewTypeUpdater(e.target.textContent.toLowerCase())}
 				className={`${
-					activeViewType === 'done' ? 'bg-green-400' : ''
-				}  flex-1 text-center h-full`}
+					activeViewType === 'done' ? 'bg-green-400 font-bold' : ''
+				}  ${selectorClasses}`}
 			>
 				Done
 			</li>
 			<li
 				onClick={(e) => viewTypeUpdater(e.target.textContent.toLowerCase())}
 				className={`${
-					activeViewType === 'left' ? 'bg-red-400' : ''
-				}  flex-1 text-center h-full`}
+					activeViewType === 'left' ? 'bg-red-400 font-bold' : ''
+				}  ${selectorClasses}`}
 			>
 				Left
 			</li>
